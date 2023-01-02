156c85235baa11e5f4399de2723dbbdfc462554d
Clemence <penin.clemence@gmail.com>
2023-01-02 12:26:40 +0100

    modif images des produits

--- src/catalog.js
+++ src/catalog.js
@@ -1,38 +1,38 @@
 const catalog = {
-  "Simsy": {
-    name: `Simsymbres`,
-    price: 11.20,
-    image: 'simsymbre.jpg'
-  },
-  "lezar": {
-    name: 'Lezard',
-    price: 22.90,
-    image: 'lezard.jpg'
-  },
-  "lethe": {
-    name: 'Eau du fleuve Léthé',
-    price: 1099.90,
-    image: 'eau-lethe.jpg'
-  },
-  "venin": {
-    name: `Venin d'acromentule`,
-    price: 279,
-    image: 'venin-acromentule.jpg'
+  "aspho": {
+    name: `Racine d'asphodèle`,
+    price: 7.90,
+    image: 'asphodele.png'
   },
   "chryso": {
     name: `Chrysopes séchées`,
     price: 6.90,
-    image: 'chrysopes.jpg'
+    image: 'chrysopes.png'
   },
-  "aspho": {
-    name: `Racine d'asphodèle`,
-    price: 7.90,
-    image: 'asphodele.jpg'
+  "Simsy": {
+    name: `Simsymbres`,
+    price: 11.20,
+    image: 'simsymbres.png'
   },
   "licor": {
     name: `Crins de licorne`,
     price: 899.90,
-    image: 'crin-licorne.jpg'
+    image: 'crins-licorne.png'
+  },
+  "lezar": {
+    name: 'Lezard',
+    price: 22.90,
+    image: 'lezard.png'
+  },
+  "venin": {
+    name: `Venin d'acromentule`,
+    price: 279,
+    image: 'venin-acromentule.png'
+  },
+  "lethe": {
+    name: 'Eau du fleuve Léthé',
+    price: 1099.90,
+    image: 'eau-lethe.png'
   },
 }
 
